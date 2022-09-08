cc3df4165e63e83f9951b871dbff85b195884d37
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-08 06:24:23 -0700

    program to print in the std error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for our program
+ *
+ * @params void
+ * Return: always return 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
