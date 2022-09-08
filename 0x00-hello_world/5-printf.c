08accc28bb8eb3abe4ba34a47c1e46616b14fc1a
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-08 04:57:26 -0700

    using printf with escape squences

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for our program
+ * @params void
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
