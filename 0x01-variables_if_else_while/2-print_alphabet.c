69093da0e36092518b43359113ab8e9565ca0237
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-09 15:50:30 -0700

    print all alphabet from a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for our program
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
