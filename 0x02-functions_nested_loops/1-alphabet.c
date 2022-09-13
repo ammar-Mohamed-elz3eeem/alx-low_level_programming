2e342dad40d48947ed39b9f9eb0cd0548414e59e
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-13 03:55:46 -0700

    making the print_alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all letters from a to z
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define FILE_LIB
 
 int _putchar(char ch);
+void print_alphabet(void);
 
 #endif
