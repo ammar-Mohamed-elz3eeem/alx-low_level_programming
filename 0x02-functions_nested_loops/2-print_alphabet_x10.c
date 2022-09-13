3b2f671de0f395acac074fcd432cff45d426388a
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-13 04:05:04 -0700

    making the print_alphabet_10x function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabets ten times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		num++;
+	}
+}
