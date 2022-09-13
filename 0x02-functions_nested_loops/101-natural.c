29ad1114176856fa1fa071278eaa74453d83ce89
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-13 16:01:08 -0700

    Sum of all natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int max = 1024;
+	int current = 1;
+	int sum = 0;
+
+	while (current < 1024)
+	{
+		if (current % 5 == 0 || current % 3 == 0)
+		{
+			sum += current;
+		}
+		current += 1;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
