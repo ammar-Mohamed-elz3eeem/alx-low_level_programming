7ac19645760f719df03c6e402e701521f78e6cd0
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-13 16:41:54 -0700

    adding fibancci till 98 script

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+        double first = 1;
+        double second = 2;
+        int numbers = 2;
+        double current;
+
+        printf("%.0f, %.0f, ", first, second);
+        while (numbers < 99)
+	{
+		current = first + second;
+		printf("%.0f", current);
+		if (numbers != 98)
+		{
+			printf(", ");
+		}
+		first = second;
+		second = current;
+		numbers += 1;
+	}
+        putchar('\n');
+        return (0);
+}
