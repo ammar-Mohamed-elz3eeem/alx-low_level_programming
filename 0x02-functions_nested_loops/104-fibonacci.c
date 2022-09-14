5e7f332e5229133954aa8b33bb24e3b95acb7f5d
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-13 17:06:42 -0700

    fixing fibancci till 98 script

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-	long double first = 1;
-	long double second = 2;
+	unsigned long double first = 1;
+	unsigned long double second = 2;
 	int numbers = 2;
-	long double current;
+	unsigned long double current;
 
-	printf("%.0f, %.0f, ", first, second);
+	printf("%.0Lf, %.0Lf, ", first, second);
 	while (numbers < 98)
 	{
 		current = first + second;
-		printf("%.0f", current);
+		printf("%.0Lf", current);
 		if (numbers != 97)
 		{
 			printf(", ");
