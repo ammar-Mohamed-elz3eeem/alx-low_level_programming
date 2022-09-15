11845acfa20f0499e33c262bed5301eeb1f90e45
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-15 16:29:32 -0700

    add print_diagonal function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal
+ * @n: int represent number of its length
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int row = 0;
+
+	while (row < n)
+	{
+		int col = 0;
+
+		while (col < row)
+		{
+			_putchar(' ');
+			col++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		row++;
+	}
+}
