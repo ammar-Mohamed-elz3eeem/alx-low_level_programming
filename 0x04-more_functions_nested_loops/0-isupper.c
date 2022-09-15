8d3844167ae590b71fbc10bf36b8a739fe8bf800
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-15 07:18:51 -0700

    add the function to check for upper case charchters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @ch: charchter to check
+ * Return: 1 if is uppercase, 0 otherwise
+ */
+
+int _isupper(int ch)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
