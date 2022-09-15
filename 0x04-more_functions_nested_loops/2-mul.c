2396f191efc75bd6b875220f3db023381c230201
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-15 07:26:30 -0700

    add mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples two functions together
+ * @num1: first number
+ * @num2: second number
+ * Return: the result of multiplication
+ */
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
