a6eebf0ee4065b4cdefac71020b540198e8573d5
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-15 17:05:24 -0700

    add fizzbuzz script

---
+++ 0x03-debugging/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int number = 1;
+	
+	while (number <= 100)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			printf("FizzBuzz");
+		else if (number % 3 == 0)
+			printf("Fizz");
+		else if (number % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", number);
+		if (number != 100)
+			putchar(' ');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
