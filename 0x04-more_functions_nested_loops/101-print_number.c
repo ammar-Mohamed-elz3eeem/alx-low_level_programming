79fbc364b118d0c98204ed6eb63c3a9f73e93fc7
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-16 06:50:41 -0700

    add print_number function

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - prints any number using only putchar
+ * @n: takes input and prints every char on it
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = 0 - n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+}
