0688cadacadbc24367c9edb012b6b99591352b69
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-19 16:37:17 -0700

    add _putchar to main.h & add _puts function:

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print the string to the stdout
+ * @str: string to print to stdout
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+}
