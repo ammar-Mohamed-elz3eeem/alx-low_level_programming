7bf7621d3e1d2346fa57d2862dfde1bb0b271124
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-19 16:16:02 -0700

    add swap_int function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two ineteger variables
+ * @n1: first integer
+ * @n2: second integer
+ */
+
+void swap_int(int *n1, int *n2)
+{
+	*n1 = *n2;
+	*n2 = *n1;
+}
