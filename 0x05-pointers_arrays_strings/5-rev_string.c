3f0133e3129a217282a5e9900aaf7f123bacd61f
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-20 18:16:04 -0700

    fix out of range error

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,14 +2,14 @@
 
 /**
  * rev_string - function to change string and reverse it
- * @str: chars array
+ * @org_str: chars array
  */
 
 void rev_string(char *org_str)
 {
 	int len = 0;
 	int counter = 0;
-	
+
 	while (org_str[len])
 		len++;
 	while (len > counter)
