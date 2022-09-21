59c693205c5fab89130ba22e5420a33cbe7cd3c4
Ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-20 18:33:27 -0700

    fix betty docs

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,14 @@
 
 void puts2(char *str)
 {
-	while (*str)
+	int i = 0;
+	
+	while (str[i] != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
-	_putchar('\n');
 }
