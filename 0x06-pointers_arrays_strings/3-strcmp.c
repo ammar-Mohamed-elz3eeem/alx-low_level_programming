5fdde9ab47a283f9fb8120809ed65e6f0e837be9
ammar-mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-22 16:31:22 +0000

    add _strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two string together
+ * @s1: first string
+ * @s2: second string to compare with
+ * Return: 0 if two strings are equal
+ * if the strings are not equal:
+ * returns the difference between the unequaled chars
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int isEqual = 0;
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return *s1 - *s2;
+		s1++;
+		s2++;
+	}
+	return 0;
+}
