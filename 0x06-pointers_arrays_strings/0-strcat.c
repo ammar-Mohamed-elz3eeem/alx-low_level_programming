b7016167183a95011037f63df329b6668ed8613b
ammar-mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-22 14:25:45 +0000

    add strcat function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatinate towo strings together
+ * @dest: string to concate to
+ * @src: string to concate from
+ * Return: concatinated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+
+	while(dest[len] != '\0')
+		len++;
+
+	while(*src)
+	{
+		*(dest + len) = *src;
+		src++;
+		len++;
+	}
+
+	return (dest);
+}
