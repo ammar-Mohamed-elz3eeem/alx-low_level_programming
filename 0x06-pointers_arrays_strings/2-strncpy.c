ea22fe1c2bf7ae5482d69004341b73420780d9e6
ammar-mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-22 15:34:43 +0000

    add _strncpy function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string according to specified number of chars
+ * @dest: string to copy to
+ * @src: string to copy from
+ * @n: number of charchters you need to copy from @src
+ * Return: the new copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+	int counter = 0;
+
+	while (*dest && counter < n)
+	{
+		*(dest + counter) = *(src + counter);
+		counter++;
+	}
+
+	return (dest);
+}
