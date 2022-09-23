0d22b447d9f7a18eb458288f903e261261ae7820
ammar-mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-23 15:37:13 +0000

    add _putchar istead of putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,11 +10,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n = -n;
-		putchar('-');
+		_putchar('-');
 	}
 	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	putchar('0' + n % 10);
+	_putchar('0' + n % 10);
 }
