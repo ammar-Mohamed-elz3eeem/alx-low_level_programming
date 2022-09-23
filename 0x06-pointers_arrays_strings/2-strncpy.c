f1325304f344b4891a89e431690001ca7bbbe8fb
ammar-mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-23 00:36:36 +0000

    refactor code for strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,35 +7,23 @@
  * @n: number of charchters you need to copy from @src
  * Return: the new copied string
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int counter = 0;
-	int len_s2 = 0;
-	int len = 0;
 
-	while (dest[len] != '\0')
-		len++;
-
-	while (src[len_s2] != '\0')
-		len_s2++;
+	while (counter < n && *src != '\0')
+	{
+		dest[counter] = *src;
+		counter++;
+		src++;
+	}
 
 	while (counter < n)
 	{
-		if ((counter) > len_s2)
-		{
-			*(dest + counter) = '\0';
-		}
-		else
-			*(dest + counter) = *(src + counter);
+		dest[counter] = '\0';
 		counter++;
 	}
-	if ((counter + len_s2) >= len)
-	{
-		while (len > counter)
-		{
-			*(dest + counter) = '\0';
-			counter++;
-		}
-	}
+
 	return (dest);
 }
