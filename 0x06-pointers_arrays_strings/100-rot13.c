49fef1a0c2a1c507f474ed998a84d45d13e61ea9
ammar-mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-23 13:11:45 +0000

    Add new tasks + add rot13 function + adding new function to main.h

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - ciphear any word using rot13 encryption
+ * @str: string to cipher
+ * Return: encrypted or decrypted string
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+	int start_char;
+
+	while (str[i])
+	{
+		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				start_char = 97;
+			else
+				start_char = 65;
+
+			int old_char = str[i];
+			int new_char = (((str[i] - start_char) + 13) % 26) + start_char;
+
+			str[i] = new_char;
+			break;
+		}
+		i++;
+	}
+	return (str);
+}
