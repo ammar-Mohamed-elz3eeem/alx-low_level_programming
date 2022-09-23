fcdb22405a8be8b52c7c22b022e51dc99ebd5c05
ammar-mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-23 15:39:23 +0000

    fix the error of don't change the orginal var

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,18 @@
 
 void print_number(int n)
 {
+	unsigned int num = 0;
+
 	if (n < 0)
 	{
-		n = -n;
+		num = -n;
 		_putchar('-');
 	}
+	else
+		num = n
 	if (n / 10)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	_putchar('0' + n % 10);
+	_putchar('0' + num % 10);
 }
