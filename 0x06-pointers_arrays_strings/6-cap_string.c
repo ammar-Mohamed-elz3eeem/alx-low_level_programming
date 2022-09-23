540c5faff6468559630211344ca3036ea93dacae
ammar-mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-09-23 01:30:19 +0000

    add cap_string function

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize every word of any word
+ * that is seperated with (space, newline, tab, ',', ';'
+ * '.', '!', '?', '"', '(', ')', '{' or '}'
+ * @str: string to operate on
+ * Return: string after capitalizing
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] == ' ' ||
+				str[index] == '\t' ||
+				str[index] == '\n' ||
+				str[index] == ',' ||
+				str[index] == ';' ||
+				str[index] == '.' ||
+				str[index] == '!' ||
+				str[index] == '\?' ||
+				str[index] == '\"' ||
+				str[index] == '(' ||
+				str[index] == ')' ||
+				str[index] == '{' ||
+				str[index] == '}')
+		{
+			if (str[index + 1] >= 'a' && str[index + 1] <= 'z')
+				str[index + 1] -= 32;
+		}
+		index++;
+	}
+	return (str);
+}
