b60872f3cc631176c736d392726a14e842322705
root <root@DESKTOP-BONIH80>
2022-09-27 01:36:59 +0200

    add _strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - returns a pointer to the first occurance of char c in string c
+ * @s: string to search on
+ * @c: charchter to search for
+ * Return: pointer to the first occurance of the char c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
