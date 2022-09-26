03d19961c332564ec6fc735800699dcd4f1c9568
root <root@DESKTOP-BONIH80>
2022-09-27 00:59:54 +0200

    add function memeset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n charchter with blank byte
+ * @s: string to fill
+ * @b: byte to put in the string
+ * @n: number of charchter to insert in the string @s
+ * Return: the string after being filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i = 0;
+    while (i < n)
+    {
+        *(s + i) = b;
+        i++;
+    }
+
+    return s;
+}
