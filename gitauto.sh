9ca263da2acfbd55ffb8eeef0b4d18a1ef482996
root <root@DESKTOP-BONIH80>
2022-09-27 01:00:33 +0200

    add script to automate git commands

---
+++ gitauto.sh
@@ -0,0 +1,13 @@
+if [ "$1" = "" ]; then
+    echo "you didn't specify path to push"
+fi
+
+if [ "$2" = "" ]; then
+    echo "you didn't entered commit message"
+fi
+
+git add $1
+
+git commit -m "$2"
+
+git push
