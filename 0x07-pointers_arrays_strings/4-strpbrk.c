f9e1c60f0a23c028d703d89b6aac73ba0599d0fd
root <root@DESKTOP-BONIH80>
2022-09-27 20:12:39 +0200

    fix betty docs & style

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,11 +2,11 @@
 #include <stddef.h>
 
 /**
- * _strpbrk - locates the first occurrence in the string 
+ * _strpbrk - locates the first occurrence in the string
  * s of any of the bytes in the string accept
  * @s: string to search on
  * @accept: string to search of its charchters
- * Return: Returns a pointer to the byte in s that matches 
+ * Return: Returns a pointer to the byte in s that matches
  * one of the bytes in accept, or NULL if no such byte is found
  */
 
@@ -27,8 +27,8 @@ char *_strpbrk(char *s, char *accept)
 			accept_char++;
 		}
 		if (i > 0)
-			return s;
+			return (s);
 		s++;
 	}
-	return NULL;
+	return (NULL);
 }
