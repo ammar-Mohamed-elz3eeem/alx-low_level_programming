789265f39cc6db276415d6cc23d0a262f2254ea9
root <root@DESKTOP-BONIH80>
2022-09-27 04:04:49 +0200

    add _strspn function

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - find the first pos of a char in string that matches
+ * the first charchter on accept
+ * @s: string to search on
+ * @accept: string to search of its charchters
+ * Return: position of the non prefix string
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int curr = 0;
+
+	while (s[curr] != '\0')
+	{
+		unsigned int i = 0;
+		unsigned int j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[curr])
+			{
+				++i;
+				break;
+			}
+			j++;
+		}
+		if (i == 0)
+			return (curr);
+		curr++;
+	}
+	return (curr);
+}
