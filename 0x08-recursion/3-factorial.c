a0b9b6340cacd5bfe866150f6339b841af656505
root <root@DESKTOP-BONIH80>
2022-09-29 00:09:29 +0200

    add factorial function

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,17 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * factorial - get the factorial of any input number
+ * @n: number to calc factorial for
+ * Return: the calculated factorial of the input number
  */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
