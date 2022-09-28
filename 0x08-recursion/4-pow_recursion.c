f405a36437859a50ac57f5edd62766aed374c3c5
root <root@DESKTOP-BONIH80>
2022-09-29 00:19:12 +0200

    add _pow_recursion function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _pow_recursion - raise the first number to the power of the second number
+ * @x: base number to calculate the power for
+ * @y: the power to raise for the x number
+ * Return: calculated result of x to the power of y
  */
+
+int _pow_recursion(int x, int y)
+{
+	if(y < 0)
+		return -1;
+	else if (y == 0)
+		return 1;
+	else
+		return x * _pow_recursion(x, y - 1);
+}
