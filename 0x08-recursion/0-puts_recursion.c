e4216484834bec1d7ace1cdd764f80223dab95bf
root <root@DESKTOP-BONIH80>
2022-09-28 20:50:41 +0200

    add _puts_recursion function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _puts_recursion - print every char on the given string
+ * @s: string to print from
  */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s)
+	{
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
