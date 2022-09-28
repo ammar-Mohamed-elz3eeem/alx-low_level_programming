7c826caa74a03af6dde1bac26e8a4c413dfaf9f5
root <root@DESKTOP-BONIH80>
2022-09-29 01:48:09 +0200

    add is_prime_number function

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,30 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * is_prime_number - check if entered number is prime or not
+ * @n: number to check
+ * Return: 1 if number is prime, 0 otherwise
  */
+
+int is_prime_number(int n)
+{
+	if (n <= 0 || n == 1)
+		return (0);
+	return (is_prime(n, 2));
+}
+
+/**
+ * is_prime_number - check if entered number is prime or not
+ * @n: number to check
+ * @denom: denomerator to determine if number can be divded by it or not
+ * Return: 1 if number is prime, 0 otherwise
+ */
+
+int is_prime(int n, int denom)
+{
+	if (n % denom == 0)
+		return (0);
+	else if (denom == n - 1)
+		return (1);
+	is_prime(n, denom + 1);
+}
