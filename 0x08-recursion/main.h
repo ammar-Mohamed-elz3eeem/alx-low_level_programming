ba44698c19d3867ecea13ed9a6e46b604b9864ab
root <root@DESKTOP-BONIH80>
2022-09-29 01:45:18 +0200

    add is_prime function to main file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,8 +7,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt(int x, int y);
 int is_prime_number(int n);
 int _putchar(char c);
+int _sqrt(int x, int y);
+int is_prime(int n, int denom);
 
 #endif
