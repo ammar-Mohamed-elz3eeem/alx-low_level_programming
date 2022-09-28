bc2a7d96abd2ff6117dba5283b0c65e19478a486
root <root@DESKTOP-BONIH80>
2022-09-29 00:02:50 +0200

    add _strlen_recursion function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strlen_recursion - get the number of the charchters on the string
+ * @s: string to count charchters
+ * Return: the number of chars on string
  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return 0;
+	else
+		return 1 + _strlen_recursion(s + 1);
+}
