ba492d1d6662c5dae57536f25abaac40c6f8a728
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-01 01:49:19 +0200

    Fivth EX

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of command-line arguments
+ * @argv: array of strings represent command line arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int i;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[i]);
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
