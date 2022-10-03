85c670062b0d7c0046db521654aab4eef6e2ef7c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-04 01:44:25 +0200

    fix betty style & docs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 	new_str = malloc(sizeof(char) * (len1 + len2) + 1);
 
 	if (new_str == NULL)
-		return NULL;
+		return (NULL);
 
 	for (len1 = 0; s1[len1]; len1++)
 		new_str[nlen++] = s1[len1];
@@ -38,5 +38,5 @@ char *str_concat(char *s1, char *s2)
 	for (len2 = 0; s2[len2]; len2++)
 		new_str[nlen++] = s2[len2];
 
-	return new_str;
+	return (new_str);
 }
