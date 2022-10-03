fceaef305f5a2bf503b5b9ce4e04f5ff77b4c84f
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-04 00:34:24 +0200

    add str_concat function

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: return newly allocated space in memory
+ * which contains the contents of s1,
+ * followed by the contents of s2, and null terminated
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1 = 0;
+	int len2 = 0;
+	int nlen;
+	char *new_str;
+
+	while (*(s1 + len1) != '\0')
+		len1++;
+	while (*(s2 + len2) != '\0')
+		len2++;
+
+	new_str = malloc(sizeof(char) * (len1 + len2) + 1);
+
+	for (nlen = 0; nlen < (len1 + len2); nlen++)
+	{
+		if (*s1)
+		{
+			new_str[nlen] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[nlen] = *s2;
+			s2++;
+		}
+	}
+
+	new_str[nlen] = '\0';
+
+	return new_str;
+}
