586819adb814bd9ca9b8059ca06dd3fabd532a64
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-04 06:01:45 +0200

    fix argstostr function

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: number of arguments passed to the program
+ * @av: array of strings represents the command-line arguments
+ * Return: string for every word in command-line args array
+ * seperated by newline
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	unsigned int i, j, len = 0, newlen = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * len);
+
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[newlen] = av[i][j];
+			newlen++;
+		}
+		str[newlen++] = '\n';
+	}
+
+	return (str);
+}
