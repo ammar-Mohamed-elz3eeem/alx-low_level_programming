787ded9f937d99369ce2acd49b95ab34289c377e
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-06 11:18:16 +0200

    fix memory default value

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,15 +12,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
+	unsigned int i;
 
-	if (!nmemb || !size)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(size * nmemb);
 
-	if (!ptr)
+	if (ptr == NULL)
 		return (NULL);
+
+	for(i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 
 	return (ptr);
 }
