61e13a021e0f067fc6967a54d51c03a92f1bfc4a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-11 00:27:20 +0200

    fix betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef DOG_FILE
 #define DOG_FILE
 
+/**
+ * struct dog - this is struct for dogs
+ * @name: Dog name
+ * @age: Dog Age
+ * @owner: Dog's Owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 
 #endif
