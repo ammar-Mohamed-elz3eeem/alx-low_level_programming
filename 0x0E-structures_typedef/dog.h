187a360b91434027fe6d03f06223f612da41dd7c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-11 04:57:36 +0200

    add new functions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,12 @@ struct dog
 	char *owner;
 };
 
+dog_t *new_dog(char *name, float age, char *owner);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 typedef struct dog dog_t;
+char *_strcpy(char *dest, char *src);
+unsigned int _strlen(char *src);
 
 #endif
