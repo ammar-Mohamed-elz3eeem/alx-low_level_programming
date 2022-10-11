13b20cea05cf63e1ee0dc616f9d80fc820e171f3
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-11 04:56:10 +0200

    fix malloc errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,13 +13,64 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
 
-	ndog = malloc(sizeof(name) + sizeof(age) + sizeof(owner));
+	ndog = malloc(sizeof(dog_t));
 
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = name;
+
+	ndog->name = malloc(sizeof(char) * _strlen(name) + 1);
+	if (ndog->name == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+
+	ndog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
+	if (ndog->owner == NULL)
+	{
+		free(ndog->owner);
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+
+	ndog->name = _strcpy(ndog->name, name);
 	ndog->age = age;
-	ndog->owner = owner;
+	ndog->owner = _strcpy(ndog->owner, owner);
 
 	return (ndog);
+}
+
+/**
+ * _strcpy - copy from src string to dest string
+ * @dest: string to copy to
+ * @src: string to copy from
+ * Return: the copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+
+	return dest;
+}
+
+/**
+ * _strlen - get string length
+ * @src: string to get its length
+ * Return: the length of string
+ */
+
+unsigned int _strlen(char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		;
+
+	return i;
 }
