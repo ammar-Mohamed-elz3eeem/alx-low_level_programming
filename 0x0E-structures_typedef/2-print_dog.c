28595e130349b719725270c5232b5bf4cccd97f1
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-11 04:32:29 +0200

    fix Null values

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,21 +12,21 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: ");
-		if ((*d).name != NULL)
-			printf("%s\n", d->name);
-		else
+		if ((*d).name == NULL)
 			printf("(nil)\n");
+		else
+			printf("%s\n", d->name);
 
 		printf("Age: ");
-		if ((*d).age != 0.0f)
-			printf("%f\n", d->age);
-		else
+		if ((*d).age < 0)
 			printf("(nil)\n");
+		else
+			printf("%f\n", d->age);
 
 		printf("Owner: ");
-		if ((*d).owner != NULL)
-			printf("%s\n", d->owner);
-		else
+		if ((*d).owner == NULL)
 			printf("(nil)\n");
+		else
+			printf("%s\n", d->owner);
 	}
 }
