61a2bcb0474869fbd326c9a34e76bdb55e2ee1a9
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-18 15:33:38 +0200

    add print_numbers function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints all numbers given in argument list
+ * @n: number of arguments passed to the function
+ * @separator: string to be printed between numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n) && i)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", va_arg(args, int));
+	}
+	printf("\n");
+}
