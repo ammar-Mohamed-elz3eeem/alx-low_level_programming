b0aae6df64e5788a15bbfb98fe369bece85aa911
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-27 23:39:20 +0200

    add get_endianness function declration

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - find if program is big endianness or little endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int i = 5;
+	char *ptr = (char *)&i;
+	if (ptr[0] == 5)
+		return 1;
+	else
+		return 0;
+}
