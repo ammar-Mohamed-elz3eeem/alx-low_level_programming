0d0f61461dc2ccbd1e258a5d077e423737cd9eb5
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-11-01 02:11:05 +0200

    add all functions to header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define FILESIO_MAIN
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
