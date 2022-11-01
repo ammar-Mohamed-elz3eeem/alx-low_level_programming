8ad7f2774380979cf5becf191e0b4bc2f6fd6038
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-11-01 02:30:25 +0200

    set stdout as posix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,6 +17,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * (letters + 1));
 	len = read(file_handler, buffer, letters);
 
-	printf("%s", buffer);
+	write(STDOUT_FILENO, buffer, len);
 	return len;
 }
