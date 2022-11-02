b1ebd81f55993369797fdc58a0e2062aec0615a3
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-11-02 02:43:38 +0200

    remove the text_content case

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,6 @@ int create_file(const char *filename, char *text_content)
 
 	file_handler = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (text_content == NULL)
-		return (-1);
-
 	while (text_content[len] != '\0')
 		len++;
 
