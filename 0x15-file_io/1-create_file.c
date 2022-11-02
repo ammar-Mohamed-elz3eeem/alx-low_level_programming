783c15e9909ae3c6c8b7c463556ea91344dacc6d
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-11-02 02:48:52 +0200

    fix code order

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,19 +11,18 @@ int create_file(const char *filename, char *text_content)
 {
 	int file_handler, file_writer, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+	if (filename != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+	}
 
 	file_handler = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	while (text_content[len] != '\0')
-		len++;
-
 	file_writer = write(file_handler, text_content, len);
 
 	if (file_handler <= 0 || file_writer <= 0)
 		return (-1);
 
 	close(file_handler);
-	return 1;
+	return (1);
 }
