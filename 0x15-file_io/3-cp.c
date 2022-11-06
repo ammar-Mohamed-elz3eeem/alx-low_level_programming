a571c1fb2107a2dade9a2bc1a5eedadddba2b4fc
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-11-06 12:18:41 +0200

    check return value for file_from & file_to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,10 +10,9 @@
 
 int main(int argc, char **argv)
 {
-	int file_from, file_to, close_dest, close_src;
+	int file_from, file_to, writer, close_dest, close_src;
 	char *buffer;
 	ssize_t len = 0;
-	int total = 0;
 
 	if (argc != 3)
 	{
@@ -22,40 +21,42 @@ int main(int argc, char **argv)
 	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	if (file_from < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+
 	buffer = malloc(sizeof(char) * 1024);
 	while ((len = read(file_from, buffer, 1024)) > 0)
 	{
-		write(file_to, buffer, len);
-		total += len;
+		if (len < 0 || file_from < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		writer = write(file_to, buffer, len);
+		if (file_to < 0 || writer < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-	close_dest = close(file_from);
-	close_src = close(file_to);
 
-	check_close_file_handler(close_dest, &file_from);
-	check_close_file_handler(close_src, &file_to);
+	close_file(&file_from);
+	close_file(&file_to);
 
 	return (0);
 }
 
 /**
- * check_close_file_handler - check if file is closed or not
+ * close_file - check if file is closed or not
  * @handler_closer: the integer number returned by the close function
  * @file_handler: the file that you want to close
  */
 
-void check_close_file_handler(int handler_closer, int *file_handler)
+void close_file(int *file_handler)
 {
-	if (handler_closer < 0)
+	int closer;
+
+	closer = close(*file_handler);
+
+	if (closer < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *file_handler);
 		exit(100);
