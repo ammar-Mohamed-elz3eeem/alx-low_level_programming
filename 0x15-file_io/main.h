ca9a576bf994b23fe89dcd87f2605da2dab444d4
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-11-06 12:21:08 +0200

    update functions

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,6 @@
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
-void check_close_file_handler(int handler_closer, int *file_handler);
+void close_file(int *file_handler);
 
 #endif
