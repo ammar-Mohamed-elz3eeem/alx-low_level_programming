9ab58947387111927bc3a209c6bcf1612c6ac0f0
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-10 15:33:38 +0200

    add node to linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add element in the linked list
+ * @head: doubly linked list
+ * @n: value to be saved on the added list item
+ * Return: the address of the new element if added, otherwise NULL
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *list = *head;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
