a887582a9b0188a33807b79562f4bb23310fb430
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-05 22:57:39 +0200

    add function to add to hashtable

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - insert element into hash table
+ * @ht: hash_table element
+ * @key: the key of the element to be inserted into hash table
+ * @value: value assosicated with that key in the hash table
+ * Return: 1 if inertion, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newEl;
+	unsigned long int i = 0, index = key_index((unsigned char *)key, ht->size);
+	char *value_dup = strdup(value);
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	if (value_dup == NULL)
+		return (0);
+
+	for (; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			ht->array[i]->value = value_dup;
+			return (1);
+		}
+	}
+
+	newEl = malloc(sizeof(hash_node_t));
+	if (newEl == NULL)
+	{
+		free(value_dup);
+		return (0);
+	}
+	newEl->key = strdup(key);
+	if (newEl->key == NULL)
+	{
+		free(newEl);
+		return (0);
+	}
+	newEl->value = value_dup;
+	newEl->next = ht->array[index];
+	ht->array[index] = newEl;
+
+	return (1);
+}
