8faaaed87a8c8bd944a4587b68b5d832e720fe3d
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-07 19:13:47 +0200

    add hash_table_print function

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function to print hashtable values
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	int flag = NO_FLAG;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			flag = FALG;
+		}
+	}
+	printf("}\n");
+}
