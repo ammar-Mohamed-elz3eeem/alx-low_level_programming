8b05e82ffc404cc341c7d0349a957f21c2837af0
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-07 19:26:12 +0200

    add function to free all mempry for hashtable

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete hashtable object
+ * ht: hashtable
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				hash_node_t *tmp = node->next;
+				free(node->value);
+				free(node->key);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
