653d0762d667e37110f5a0f5b252e08b6dcd353f
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-23 19:45:05 +0200

    add island perimeter algorithm

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" get the perimeter of the island """
+
+
+def island_perimeter(grid):
+        perimeter = 0
+        
+        for row in range(1, len(grid)):
+                for col in range(1, len(grid[row])):
+                        if grid[row][col] == 1:
+                                if grid[row - 1][col] == 0:
+                                        perimeter += 1
+                                if grid[row][col - 1] == 0:
+                                        perimeter += 1
+                                if grid[row + 1][col] == 0:
+                                        perimeter += 1
+                                if grid[row][col + 1] == 0:
+                                        perimeter += 1
+        if grid[0][0] == 1:
+                perimeter += 2
+        if grid[0][len(grid[0]) - 1] == 1:
+                perimeter += 2
+        return (perimeter)
