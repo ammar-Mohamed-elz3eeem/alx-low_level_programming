fce5ec7b80148589699e0c32064642f988a9d808
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-23 19:57:47 +0200

    fix the calculations

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,20 +14,15 @@ def island_perimeter(grid):
     """
 
     perimeter = 0
+    edges = 0
 
-    for row in range(1, len(grid)):
-        for col in range(1, len(grid[row])):
-            if grid[row][col] == 1:
-                if grid[row - 1][col] == 0:
-                    perimeter += 1
-                if grid[row][col - 1] == 0:
-                    perimeter += 1
-                if grid[row + 1][col] == 0:
-                    perimeter += 1
-                if grid[row][col + 1] == 0:
-                    perimeter += 1
-    if grid[0][0] == 1:
-        perimeter += 2
-    if grid[0][len(grid[0]) - 1] == 1:
-        perimeter += 2
-    return (perimeter)
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col]:
+                perimeter += 1
+                
+                if row > 0 and grid[row - 1][col]:
+                    edges += 1
+                if col > 0 and grid[row][col - 1]:
+                    edges += 1
+    return perimeter * 4 - edges * 2
