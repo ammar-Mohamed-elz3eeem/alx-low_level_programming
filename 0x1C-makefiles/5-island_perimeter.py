07b33f9afc386968435570cf6af0dc806d0efdd8
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-23 19:50:43 +0200

    fix bycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,21 +3,31 @@
 
 
 def island_perimeter(grid):
-        perimeter = 0
-        
-        for row in range(1, len(grid)):
-                for col in range(1, len(grid[row])):
-                        if grid[row][col] == 1:
-                                if grid[row - 1][col] == 0:
-                                        perimeter += 1
-                                if grid[row][col - 1] == 0:
-                                        perimeter += 1
-                                if grid[row + 1][col] == 0:
-                                        perimeter += 1
-                                if grid[row][col + 1] == 0:
-                                        perimeter += 1
-        if grid[0][0] == 1:
-                perimeter += 2
-        if grid[0][len(grid[0]) - 1] == 1:
-                perimeter += 2
-        return (perimeter)
+    """
+        island_perimeter
+
+        Args:
+            grid (list<list<int>>): list of lists that represent island
+
+        Returns:
+            int: perimeter of islamd
+    """
+
+    perimeter = 0
+
+    for row in range(1, len(grid)):
+        for col in range(1, len(grid[row])):
+            if grid[row][col] == 1:
+                if grid[row - 1][col] == 0:
+                    perimeter += 1
+                if grid[row][col - 1] == 0:
+                    perimeter += 1
+                if grid[row + 1][col] == 0:
+                    perimeter += 1
+                if grid[row][col + 1] == 0:
+                    perimeter += 1
+    if grid[0][0] == 1:
+        perimeter += 2
+    if grid[0][len(grid[0]) - 1] == 1:
+        perimeter += 2
+    return (perimeter)
