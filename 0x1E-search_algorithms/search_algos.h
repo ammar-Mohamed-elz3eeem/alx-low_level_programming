bc86f1a602cd44d21dec52cb25cc27c1438e62d5
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-13 18:52:04 +0200

    add binary_search_for in header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
-int do_binary_searching(int *array, size_t low, size_t high, int value);
+int do_binary_searching_rec(int *array, size_t low, size_t high, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t low, size_t high);
+int do_binary_searching_for(int *array, size_t high, int value);
 
 #endif
